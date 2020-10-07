3bc1cdcf1d94af3ed774054f1976bff7d83f96bf
PlushZ <polpolunina@gmail.com>
2020-10-07 17:22:37 +0500

    Task Manager

--- taskmanager/taskmanager/settings.py
+++ taskmanager/taskmanager/settings.py
@@ -19,10 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&zv8tr8e4a86hf_3mj*bjqx4%)h19*7aew=uvmjr=%uthu73&d'
+SECRET_KEY = 'secretkey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
